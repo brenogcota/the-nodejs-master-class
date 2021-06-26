5fd4381bd4247afcdaf6ad20150e19adace11bbe
brenogcota <brenocota4@gmail.com>
2021-06-25 23:51:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
+
+const server = http.createServer(function (req,res){
+  // Get the URL and parse it
+  let parseUrl = url.parse(req.url, true);
+   
+  // Get de path
+  let path = parseUrl.pathname;
+  let trimedPath = path.replace(/^\/+|\/+$/g,'');
+  
+  // Get the query string as an object
+  let queryStringObject = parseUrl.query;
+
+  // Get the HTTP Method
+  let method = req.method.toLowerCase();
+
+  // Get the headers as an object
+  let headers = req.headers;
+
+  // Get the payload, if any
+  let decoder = new StringDecoder('uft-8');
+  let buffer = '';
+  req.on('data', function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end', function() {
+    buffer +=  decoder.end();
+    
+    // Choose the handler this request should got to. if one is not
+    let chooseHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    // Construct the data object ot send to the handler;
+    let data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method': method,
+      'headers': headers,
+      'payload': buffer
+    };
+
+    // Route the request to the handler specified in the router
+    chooseHandler(data, function(statusCode, payload) {
+      // Use the status code called back by the handler, or default
+     statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+     // Use the payload called back by the handler, or default to
+     payload = typeof(payload) == object ? payload : {};
+
+     // Convert the payload to a string
+     let payloadString = JSON.stringfy(payload);
+
+ 
+      // Send the respon
+      res.writeHead(statusCode);
+      res.end('Hello, World\n');
+    
+      // Log de request path
+      console.log('Request received on path: /', trimedPath);
+      console.log(statudCode,' -- Request method: ', method);
+    });
+  });
+});
+
+server.listen(3000, function() {
+  console.log('server is running on localhost:3000 ...');
+});
+
+// Define the handlers
+let handlers = {};
+
+// Sample handler
+handlers.sample = function(data, callback) {
+  //
+  callback(406, {'name': 'sample handler'});
+};
+
+// Not found handler
+handlers.notFound = function(data, callback) {
+  callback(404);
+};
+
+// Define a request router
+let router = {
+  'sample': handlers.sample
+}
